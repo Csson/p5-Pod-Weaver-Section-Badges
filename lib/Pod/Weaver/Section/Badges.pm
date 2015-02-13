d0854d42fb7680f85940e5087fc3230da362fc90
Erik Carlsson <info@code301.com>
2015-02-13 16:04:32 +0100

    Init

---
+++ lib/Pod/Weaver/Section/Badges.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Pod::Weaver::Section::Badges;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Pod::Weaver::Section::Badges;
+
+=head1 DESCRIPTION
+
+Pod::Weaver::Section::Badges is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+use Pod::Weaver::Section::Badges;
+
+fail('this test is TODO!');
+done_testing;
