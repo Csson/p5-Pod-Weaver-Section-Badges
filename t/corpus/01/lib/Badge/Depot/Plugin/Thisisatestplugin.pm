0ccd7f180a8f56f10901af236a794ecc7f32182b
Erik Carlsson <info@code301.com>
2015-02-17 19:02:57 +0100

    Renamed test modules

---
+++ t/corpus/01/lib/Badge/Depot/Plugin/Thisisatestplugin.pm
@@ -0,0 +1,29 @@
+use 5.14.0;
+use strict;
+use warnings;
+use Moops;
+
+class Badge::Depot::Plugin::Thisisatestplugin using Moose with Badge::Depot {
+
+    has user => (
+        is => 'ro',
+        isa => Str,
+    );
+    has repo => (
+        is => 'ro',
+        isa => Str,
+    );
+    
+    method create_badge {
+        my $a_href = 'https://example.com/%s/%s';
+        my $img_href = 'https://example.com/%s/%s.svg';
+
+        my $link = sprintf $a_href => $self->user, $self->repo;
+        my $img = sprintf $img_href => $self->user, $self->repo;
+
+        return sprintf qq{<a href="%s"><img src="%s" /></a>} => $link, $img;
+    }
+    
+}
+
+1;
