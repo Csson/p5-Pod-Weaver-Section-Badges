cef195ca3c8ceed64b060bae9bf010825a0d8172
Erik Carlsson <info@code301.com>
2016-02-17 22:25:25 +0000

    Put PluginSearcher in a separate file

---
+++ lib/Pod/Weaver/Section/Badges/PluginSearcher.pm
@@ -0,0 +1,13 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package Pod::Weaver::Section::Badges::PluginSearcher;
+
+# AUTHORITY
+our $VERSION = '0.0401';
+
+use Moose;
+use Module::Pluggable search_path => ['Badge::Depot::Plugin'], require => 1;
+
+1;
